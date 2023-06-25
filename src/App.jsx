7bda132d18262885e4c81e5166005bb5f78a00db
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-06-25 15:04:58 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Blogs from './pages/Blogs';
+import Navbar from './components/Navbar';
+
+import './App.css';
+
+
+const  App=()=> {
+  
+
+  return (
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>  
+              <Route path='/' element={<Home/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/blogs' element={<Blogs/>}/>
+        </Routes>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
